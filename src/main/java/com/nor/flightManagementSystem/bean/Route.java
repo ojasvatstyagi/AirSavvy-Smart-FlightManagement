ce5bb8ab176705cf89898aede218bf7a935e771c
ojas tyagi <ojastyagi753@gmail.com>
2024-07-14 10:41:13 +0530

    version 3.0

--- src/main/java/com/nor/flightManagementSystem/bean/Route.java
+++ src/main/java/com/nor/flightManagementSystem/bean/Route.java
@@ -3,7 +3,6 @@ package com.nor.flightManagementSystem.bean;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Route {
